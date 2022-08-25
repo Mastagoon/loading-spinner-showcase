6f1ef0a6c99214aeb4a235662e68c68f7b181cd3
Mastagoon <inzilagiway@gmail.com>
2022-08-26 01:01:30 +0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { useState } from "react"
+import { Router } from "next/router"
+import Loading from "./components/Loading"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false)
+  Router.events.on("routeChangeStart", () => setLoading(true)) // when next page is loading
+  Router.events.on("routeChangeComplete", () => setLoading(false)) // once rout change is completed
+
+  return (
+    <>
+      {loading && <Loading />}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
